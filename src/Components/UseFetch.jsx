c89af36de0d8b77e5787ab4bdd47aaae64c0368b
Alejo <alejojpa2@gmail.com>
2024-10-01 08:25:13 -0400

    reusable custom hookSetting up the at UseFetch.jsx

--- src/Components/UseFetch.jsx
+++ src/Components/UseFetch.jsx
@@ -1,4 +1,15 @@
+import { useState, useEffect } from "react";
+
+//Reusable custom hook
 const UseFetch = (url) => {
+    const[data, setData] = useState();
+    
+    //to fetching data
+    useEffect(()=>{
+            fetch(url).then((res)=>res.json()) //This initiates an HTTP request 
+            .then((data)=>setData(data)) //For updating variable data, sets the retrieved data to the state variable data
+    },[])
+    return [data]
     
 }
 
