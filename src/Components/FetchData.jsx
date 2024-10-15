d990125eaa510ed836bf0a5eca21bf6e9874b2af
Alejo <alejojpa2@gmail.com>
2024-10-15 09:41:29 +0200

    adding url

--- src/Components/FetchData.jsx
+++ src/Components/FetchData.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import UseFetch from './UseFetch';
 
 const FetchData = () => {
-  const [data] = UseFetch('');
+  const [data] = UseFetch('https://api.npoint.io/9045c260b1565daa9e15');
   console.log(data);
   return (
     <>
